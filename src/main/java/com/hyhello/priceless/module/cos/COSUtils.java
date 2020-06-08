e260241693842608da636dbdcdaa408c99afa231
piao <1712439537@qq.com>
2020-06-08 19:36:45 +0800

    cos底层

--- src/main/java/com/hyhello/priceless/module/cos/COSUtils.java
+++ src/main/java/com/hyhello/priceless/module/cos/COSUtils.java
@@ -1,6 +1,7 @@
 package com.hyhello.priceless.module.cos;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.hyhello.priceless.config.TokenConfig;
@@ -10,19 +11,9 @@ import com.qcloud.cos.auth.BasicCOSCredentials;
 import com.qcloud.cos.auth.COSCredentials;
 import com.qcloud.cos.exception.CosClientException;
 import com.qcloud.cos.exception.CosServiceException;
-import com.qcloud.cos.model.Bucket;
-import com.qcloud.cos.model.COSObject;
-import com.qcloud.cos.model.COSObjectInputStream;
-import com.qcloud.cos.model.COSObjectSummary;
-import com.qcloud.cos.model.CannedAccessControlList;
-import com.qcloud.cos.model.CreateBucketRequest;
-import com.qcloud.cos.model.GetObjectRequest;
-import com.qcloud.cos.model.ListObjectsRequest;
-import com.qcloud.cos.model.ObjectListing;
-import com.qcloud.cos.model.ObjectMetadata;
-import com.qcloud.cos.model.PutObjectRequest;
-import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.*;
 import com.qcloud.cos.region.Region;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -40,102 +31,91 @@ public class COSUtils {
         this.tokenConfig = tokenConfig;
     }
 
-    public void createBucket(String bucketName) {
+    public Bucket createBucket(String bucketName) {
         //存储桶名称，格式：BucketName-APPID
-        String bucket = bucketName + "-" + tokenConfig.getCosAppId();
-        CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucket);
+        CreateBucketRequest createBucketRequest = new CreateBucketRequest(bucketName);
         // 设置 bucket 的权限为 Private(私有读写), 其他可选有公有读私有写, 公有读写
         createBucketRequest.setCannedAcl(CannedAccessControlList.PublicRead);
-        try{
-            Bucket bucketResult = cosClient.createBucket(createBucketRequest);
-        } catch (CosServiceException serverException) {
-            serverException.printStackTrace();
-        }
+
+       return cosClient.createBucket(createBucketRequest);
     }
-    
-    public void listBucket() {
-        List<Bucket> buckets = cosClient.listBuckets();
-        for (Bucket bucketElement : buckets) {
-            String bucketName = bucketElement.getName();
-            String bucketLocation = bucketElement.getLocation();
-            System.out.println(bucketName);
-        }
+
+    public List<Bucket> listBucket(String bucketName) {
+        return cosClient.listBuckets();
     }
-    
-    public void uploadObject(String bucketName) {
-     // 指定要上传的文件
-        File localFile = new File("C:\\Users\\Administrator\\Desktop\\yrzx.xlsx");
+
+    /**
+     *     同步上传对象
+     */
+    public PutObjectResult uploadObject(String bucketName, File file) {
         // 指定要上传到的存储桶
         String bucket = bucketName + "-" + tokenConfig.getCosAppId();
         // 指定要上传到 COS 上对象键
-        String key = "yrzx";
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
-        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
+        String key = file.getName();
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, file);
+        return cosClient.putObject(putObjectRequest);
     }
     
-    public void downloadObject() {
-     // Bucket的命名格式为 BucketName-APPID ，此处填写的存储桶名称必须为此格式
-        String bucketName = "examplebucket-1250000000";
-        String key = "exampleobject";
+    public COSObjectInputStream downloadObject(String bucketName, String key) {
         // 方法1 获取下载输入流
         GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, key);
         COSObject cosObject = cosClient.getObject(getObjectRequest);
-        COSObjectInputStream cosObjectInput = cosObject.getObjectContent();
 
+        return cosObject.getObjectContent();
+    }
+    public File downloadObject(String bucketName, String key, File downFile) {
         // 方法2 下载文件到本地
-        String outputFilePath = "exampleobject";
-        File downFile = new File(outputFilePath);
+        GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, key);
         getObjectRequest = new GetObjectRequest(bucketName, key);
         ObjectMetadata downObjectMeta = cosClient.getObject(getObjectRequest, downFile);
+        return downFile;
     }
-    
-    public void listObject(String bucketName) {
-     // Bucket的命名格式为 BucketName-APPID ，此处填写的存储桶名称必须为此格式
-        String bucket = bucketName + "-" + tokenConfig.getCosAppId();
+
+    public List<COSObjectSummary> listObject(String bucketName, boolean iterDir) {
+        return listObject(bucketName, "", iterDir);
+    }
+
+
+    /**
+     *   文件的路径key
+     *   String key = cosObjectSummary.getKey();
+     *   文件的etag
+     *   String etag = cosObjectSummary.getETag();
+     *   文件的长度
+     *   long fileSize = cosObjectSummary.getSize();
+     *   文件的存储类型
+     *   String storageClasses = cosObjectSummary.getStorageClass();
+     */
+    public List<COSObjectSummary> listObject(String bucketName, String prefix, boolean iterDir){
         ListObjectsRequest listObjectsRequest = new ListObjectsRequest();
         // 设置bucket名称
         listObjectsRequest.setBucketName(bucketName);
         // prefix表示列出的object的key以prefix开始
-        listObjectsRequest.setPrefix("");
+        listObjectsRequest.setPrefix(prefix);
         // deliter表示分隔符, 设置为/表示列出当前目录下的object, 设置为空表示列出所有的object
-        listObjectsRequest.setDelimiter("");
+        listObjectsRequest.setDelimiter(iterDir ? "" : "/");
         // 设置最大遍历出多少个对象, 一次listobject最大支持1000
         listObjectsRequest.setMaxKeys(1000);
         ObjectListing objectListing = null;
+
+        List<COSObjectSummary> results = new ArrayList<>();
         do {
-            try {
-                objectListing = cosClient.listObjects(listObjectsRequest);
-            } catch (CosServiceException e) {
-                e.printStackTrace();
-                return;
-            }
+            objectListing = cosClient.listObjects(listObjectsRequest);
             // common prefix表示表示被delimiter截断的路径, 如delimter设置为/, common prefix则表示所有子目录的路径
             List<String> commonPrefixs = objectListing.getCommonPrefixes();
 
             // object summary表示所有列出的object列表
             List<COSObjectSummary> cosObjectSummaries = objectListing.getObjectSummaries();
-            for (COSObjectSummary cosObjectSummary : cosObjectSummaries) {
-                // 文件的路径key
-                String key = cosObjectSummary.getKey();
-                // 文件的etag
-                String etag = cosObjectSummary.getETag();
-                // 文件的长度
-                long fileSize = cosObjectSummary.getSize();
-                // 文件的存储类型
-                String storageClasses = cosObjectSummary.getStorageClass();
-                
-                System.out.println(key);
-                System.out.println(fileSize);
-                System.out.println(storageClasses);
-            }
+            results.addAll(cosObjectSummaries);
 
             String nextMarker = objectListing.getNextMarker();
             listObjectsRequest.setMarker(nextMarker);
         } while (objectListing.isTruncated());
+
+        return results;
      }
     
     public void deleteObject(String bucketName, String key) {
-        String bucket = bucketName + "-" + tokenConfig.getCosAppId();
         cosClient.deleteObject(bucketName, key);
     }
    
